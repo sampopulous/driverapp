4284dd1b73dd65cd6200a4c0d04ce56d453bd91d
sampopulous <65770070+sampopulous@users.noreply.github.com>
2020-05-22 18:36:20 +0530

    Initial commit

---
+++ www/js/config.js
@@ -0,0 +1,9 @@
+/*Driver App Configuration*/
+
+var krms_driver_config ={			
+	'ApiUrl':"http://ojeelojee.com/driver/api",		
+	'DialogDefaultTitle':"Delivery Boy App",
+	'mapboxToken' : '',
+	'APIHasKey':"oMSjuDBkAShdssfksnd",
+	'debug': false
+};
